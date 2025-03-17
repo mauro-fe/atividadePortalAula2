9eb93b139c5b14a7a03c2aa4c1cf0a8bab9fbbd1
Mauro Felix <119742741+mauro-fe@users.noreply.github.com>
2025-03-17 12:45:41 -0300

    Adicionando nova atividade

--- index.js
+++ index.js
@@ -23,5 +23,21 @@ function dadosPessoais() {
     console.log("Endereco: " + endereco);
     console.log("Telefone: " + telefone);
 }
-dadosPessoais();
+// dadosPessoais();
 
+function trianguloEquilatero() {
+
+    function calcularAreaTrianguloEquilatero(lado) {
+        return (Math.sqrt(3) / 4) * Math.pow(lado, 2)
+    }
+    let lado = parseFloat(prompt("Por favor, digite o lado do triangulo equilátero: "))
+    let area = calcularAreaTrianguloEquilatero(lado)
+
+    console.log("A área total do triangulo equilatero com o valor passado anteriormente é " + area);
+}
+
+// trianguloEquilatero();
+
+let pow = Math.pow(5, 2);
+let sqrt = Math.sqrt((3));
+console.log(pow, sqrt)
